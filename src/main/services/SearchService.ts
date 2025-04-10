ed1fbcd38090a6769076003f0600b41ca73b0c43
Vaayne <liu.vaayne@gmail.com>
2025-04-10 12:07:17 +0800

    Make search window initially hidden

--- src/main/services/SearchService.ts
+++ src/main/services/SearchService.ts
@@ -19,7 +19,7 @@ export class SearchService {
     const newWindow = new BrowserWindow({
       width: 800,
       height: 600,
-      show: true,
+      show: false,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
